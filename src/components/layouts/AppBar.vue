99fdf621d8db630fd67aefd20b8843a2934897bc
ArcaneFoxie <arcanefoxie@gmail.com>
2024-10-22 17:21:29 +0100

    title bar

--- src/components/layouts/AppBar.vue
+++ src/components/layouts/AppBar.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-app-bar title="BetterTwitchTTS" />
+  <v-app-bar title="BetterTwitchTTS">
+    <v-spacer />
+    <v-btn variant="outlined">
+      Connect
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script setup lang="ts">
-
+  //
 </script>
 
-<style scoped lang="sass">
-
+<style lang="css">
+  .v-toolbar-title {
+    flex: unset !important;
+  }
 </style>
