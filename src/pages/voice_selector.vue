2a25eef1a316a27b38729b84ca0501fc7d3b7488
ArcaneFoxie <arcanefoxie@gmail.com>
2024-10-28 23:48:31 +0000

    voice selector UI

--- src/pages/voice_selector.vue
+++ src/pages/voice_selector.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    <span>
-      Voice Selector
-    </span>
+    <v-list>
+      <v-list-subheader>TTS VOICES</v-list-subheader>
+
+      <v-list-item
+        v-for="voice in sortedVoices"
+        :key="voice.name"
+        color="primary"
+        rounded="shaped"
+        :value="voice.name"
+      >
+        {{ voice.lang }} - {{ voice.name }}
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
 <script lang="ts" setup>
-  //
+  const voices = window.speechSynthesis.getVoices()
+
+  const sortedVoices = voices.sort((a, b) => `${a.lang} - ${a.name}`.toLowerCase().localeCompare(`${b.lang} - ${b.name}`.toLowerCase()))
+
 </script>
