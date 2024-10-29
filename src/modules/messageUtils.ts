e1c178af08f240ef271f5152a392b5c0cd5566af
ArcaneFoxie <arcanefoxie@gmail.com>
2024-10-29 08:09:05 +0000

    new function due to certain words causing invalid regex

--- src/modules/messageUtils.ts
+++ src/modules/messageUtils.ts
@@ -3,6 +3,10 @@ import tts from './tts'
 export function containsBlacklistedWord (message: string) {
   if (tts.store.tts.blacklistedWords.length === 0) { return false }
 
-  const pattern = new RegExp(`\\b(${tts.store.tts.blacklistedWords.join('|')})\\b`, 'i')
-  return pattern.test(message)
+  const blacklistSet = new Set(tts.store.tts.blacklistedWords.map(word => word.toLowerCase()))
+  const words = message.toLowerCase().match(/\b\w+\b/g)
+
+  if (!words) { return false }
+
+  return words.some(word => blacklistSet.has(word))
 }
